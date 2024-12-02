0baae9f833a73a1384908eb94e2e5fc158d41f2a
Nik Ahmad Izzat <nikizzat0309@gmail.com>
2024-12-02 13:31:34 +0800

    fix

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,9 @@ export default function Home() {
   return (
     <div className="flex flex-col overflow-hidden">
       <div className="flex justify-end gap-4 pr-10 pt-10">
-        <ThemeSwitch />
+        <div className="mt-[-2.5px]">
+          <ThemeSwitch />
+        </div>
 
         {/* GitHub Icon */}
         <Link

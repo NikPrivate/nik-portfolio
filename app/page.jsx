9809c9a98f6cd0d02f2c167b5a88d2d7e79c0556
Nik Ahmad Izzat <nikizzat0309@gmail.com>
2025-01-30 05:57:13 +0800

    done

--- app/page.jsx
+++ app/page.jsx
@@ -2,18 +2,11 @@ import Contact from "@/components/Contact";
 import Intro from "@/components/Intro";
 import Project from "@/components/Project";
 import Skills from "@/components/Skills";
-import ThemeSwitch from "@/components/ThemeSwitch";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col overflow-hidden">
-      <div className="flex justify-end gap-4 pr-10 pt-10">
-        {/* GitHub Icon */}
-      </div>
-
-      <div className="flex items-center justify-center pt-10 px-5 2xl:mt-[100px]">
+      <div className="flex items-center justify-center mt-3 xl:mt-10 lg:mt-10 md:mt-10 px-5 2xl:mt-[100px]">
         <div className="grid grid-cols-1 lg:grid-rows-2 lg:grid-cols-2 lg:grid-flow-col gap-4">
           <div className="col-span-1">
             <Intro className="mb-0" />
